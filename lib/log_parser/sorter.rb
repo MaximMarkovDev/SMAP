0ad015dbe6ad9f71aeb6fb38e4e758ea1de8d4a4
Maxim Markov <maxmar@europe.com>
2023-01-12 12:12:07 +0300

    Sorter class

--- lib/log_parser/sorter.rb
+++ lib/log_parser/sorter.rb
@@ -1,1 +1,21 @@
-# Sorter class
+# frozen_string_literal: true
+
+module LogParser
+  class Sorter
+    attr_reader :data
+
+    def initialize(data)
+      @data = data
+    end
+
+    def call
+      sort_data
+    end
+
+    private
+
+    def sort_data
+      data.each { |_key, value| value.sort_by!(&:values).reverse! }
+    end
+  end
+end
